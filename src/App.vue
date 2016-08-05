d1f5a5071602cfa283c3e12b71eb2057631a4fd1
hanks-zyh <zhangyuhan2014@gmail.com>
2016-08-05 22:16:23 +0800

    finish get video list

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,183 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+    <div class="container">
+       <div class="video-main mdl-shadow--2dp">
+           <div class="video-player">
+               <div class="video-playing">
+                 <video controls poster="{{ currentVideo ? currentVideo.coverForFeed : '#' }}">
+                   <source  src="{{ currentVideo ? currentVideo.playUrl : '#' }}" >
+               </video>
+               </div>
+               <div class="video-info">
+                   <h1>{{ currentVideo ?  currentVideo.title : 'title' }}</h1>
+                   <p>{{ currentVideo ?  currentVideo.description : 'des' }}</p>
+                   <div>{{ currentVideo ? currentVideo.category: 'TAG' }}</div>
+               </div>
+           </div>
+           <div class="video-list">
+               <ul class="video-ul">
+                   <li v-for="video in videoList">
+                       <div class="title">
+                         <span>{{ video.title }}</span>
+                       </div>
+                       <a href="#" v-on:click="changeCurrentVideo($index)">
+                         <img src="{{ video.coverForFeed }}" alt="img">
+                      </a>
+                   </li>
+               </ul>
+           </div>
+       </div>
+   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-export default {
-  components: {
-    Hello
+var Vue = require('vue')
+var vueResource = require('vue-resource')
+Vue.use(vueResource)
+export default{
+  data () {
+    return {
+      url: 'http://baobab.wandoujia.com/api/v1/feed',
+      nextPageUrl: '',
+      videoList: [],
+      currentVideo: null,
+      date: 0
+    }
+  },
+  ready () {
+    this.getData(this.url)
+  },
+  methods: {
+    getData (url) {
+      this.$http.get(url).then((response) => {
+        var json = JSON.parse(response.body)
+        this.nextPageUrl = json.nextPageUrl
+        var list = json.dailyList
+        if (list.length > 0) {
+          this.date = list[0].date
+          var videoList = list[0].videoList
+          if (videoList.length > 0) {
+            this.currentVideo = videoList[0]
+          }
+          for (var i = 0; i < videoList.length; i++) {
+            this.videoList.push(videoList[i])
+          }
+        }
+      }, (error) => {
+        console.error(error)
+      })
+    },
+    changeCurrentVideo (position) {
+      if (this.videoList) {
+        this.currentVideo = this.videoList[position]
+      }
+    }
   }
 }
+
 </script>
 
 <style>
-html {
-  height: 100%;
-}
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    ::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+        background-color: transparent;
+    }
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
+    ::-webkit-scrollbar-thumb {
+        border-radius: 3px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        background-color: rgba(189, 189, 189, 0.4);
+    }
+    .mdl-shadow--2dp {
+        -webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.2), 0 1px 5px 0 rgba(0,0,0,0.12);
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.2), 0 1px 5px 0 rgba(0,0,0,0.12);
+    }
+    html {
+        height: 100%;
+        width: 100%;
+        overflow: hidden;
+    }
 
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+    body {
+        font-size: 12px;
+        font-family: "Microsoft Yahei", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        text-rendering: optimizeLegibility;
+        background-color: #EEEEEE;
+        height: 100%;
+        width: 100%;
+        overflow: hidden;
+    }
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+    }
+    .video-main{
+        background: black;
+        height: 80%;
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .video-player{
+        display: flex;
+        width: 77%;
+        flex-direction: column;
+        align-self: stretch;
+    }
+    .video-playing{
+        background: black;
+        /*flex:5;*/
+        align-items: flex-start;
+        display: flex;
+    }
+    .video-playing>video {
+      width: 100%;
+    }
+    .video-info{
+        background: white;
+        flex:2;
+    }
+    .video-ul{
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        overflow-y: scroll;
+    }
+    .video-ul>li>a>img{
+        height: 140px;
+    }
+    .video-ul>li{
+        position: relative;
+        bottom: 0;
+    }
+    .title {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+    }
+    .title>span {
+        color: white;
+        display: block;
+        background: rgba(0,0,0,0.5);
+        text-decoration: none;
+        padding: 1em;
+        font-size: 1em;
+    }
 </style>
